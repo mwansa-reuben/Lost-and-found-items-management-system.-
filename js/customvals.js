335df76a4e511ebd8b196567110914e2b92c2d25
mwansa-reuben <73103644+mwansa-reuben@users.noreply.github.com>
2022-04-26 02:16:52 -0700

    Add files via upload

---
+++ js/customvals.js
@@ -0,0 +1,13 @@
+var
+    classroom = 0,
+    gradeId = 0,
+    subjectId = 0,
+    lessonPlanId = 0,
+    examId = 0,
+    gradeName = '',
+    className = '',
+    commentsInterval = 0,
+    isShortcut = false,
+    targetPage = '',
+    educationLevel = '',
+    studentid = 0;
